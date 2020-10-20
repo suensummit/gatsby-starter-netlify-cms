3753442422634ef6b4b484240b3eb16522d77e19
Summit Suen <summit.suen@gmail.com>
2020-10-20 22:43:35 +0800

    Modify blog-post template.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,9 +3,17 @@ import PropTypes from 'prop-types'
 import { kebabCase } from 'lodash'
 import { Helmet } from 'react-helmet'
 import { graphql, Link } from 'gatsby'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+
 
 export const BlogPostTemplate = ({
   content,
@@ -15,6 +23,17 @@ export const BlogPostTemplate = ({
   cats,
   tags,
   title,
+  author,
+  origin,
+  publisher,
+  year,
+  owner,
+  dev,
+  property,
+  signature,
+  field,
+  spec,
+  refs,
   helmet,
 }) => {
   const PostContent = contentComponent || Content
@@ -23,54 +42,127 @@ export const BlogPostTemplate = ({
     <section className="section">
       {helmet || ''}
       <div className="container content">
-        <div className="columns">
-          <div className="column is-3 is-offset-1">
-            {featuredimage ? (
-              <div className="featured-thumbnail">
-                <PreviewCompatibleImage
-                  imageInfo={{
-                    image: featuredimage,
-                    alt: `featured image thumbnail for post ${title}`,
-                  }}
-                />
-              </div>
-            ) : null}
+        {featuredimage ? (
+          <div className="full-width-image-blog">
+            <img
+              src="/img/ip-04.jpg"
+              height="300px"
+              width="100%"
+            />
           </div>
-          <div className="column is-7 is-offset-1">
+        ) : null}
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
               {title}
             </h1>
-            {cats && cats.length ? (
-              <div style={{ marginTop: `1rem` }}>
-                <ul className="catlist">
-                  {cats.map((cat) => (
-                    <li key={cat + `cat`}>
-                      <Link to={`/cats/${kebabCase(cat)}/`}>{cat}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-            {tags && tags.length ? (
-              <div style={{ marginTop: `1rem` }}>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-            <br />
-            <br />
-            <p>{logline}</p>
           </div>
         </div>
         <div className="columns">
+          <TableContainer component={Paper} className="column is-8 is-offset-1">
+            <Table aria-label="simple table">
+              <TableBody>
+                <TableRow key={title}>
+                  <TableCell className="column is-4 is-offset-1">
+                    LOGLINE ----
+                    <br />
+                    {logline}
+                  </TableCell>
+                  <TableCell className="column is-8 is-offset-1">
+                    TYPE ----
+                    <br />
+                    {cats && cats.length ? (
+                      <div style={{ marginTop: `1rem` }}>
+                        <ul className="catlist">
+                          {cats.map((cat) => (
+                            <li key={cat + `cat`}>
+                              <Link to={`/cats/${kebabCase(cat)}/`}>{cat}</Link>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ) : null}
+                    <br />
+                    KEYWORD ----
+                    <br />
+                    {tags && tags.length ? (
+                      <div style={{ marginTop: `1rem` }}>
+                        <ul className="taglist">
+                          {tags.map((tag) => (
+                            <li key={tag + `tag`}>
+                              <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ) : null}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <div className="columns">
           <div className="column is-10 is-offset-1">
-            <PostContent content={content} />
+            基本資訊 Information
           </div>
+        </div>
+        <div className="columns">
+          <TableContainer component={Paper} className="column is-8 is-offset-1">
+            <Table aria-label="simple table">
+              <TableBody>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">創作者</TableCell>
+                  <TableCell><Link to={`https://zh.wikipedia.org/wiki/${author}`}>{author}</Link></TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">原始文體</TableCell>
+                  <TableCell>{origin}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">版權擁有者</TableCell>
+                  <TableCell><Link to={`https://zh.wikipedia.org/wiki/${owner}`}>{owner}</Link></TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">出版者</TableCell>
+                  <TableCell>{publisher}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">目前開發狀態</TableCell>
+                  <TableCell>{dev}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            詮釋資訊 Highlights
+          </div>
+        </div>
+        <div className="columns">
+          <TableContainer component={Paper} className="column is-8 is-offset-1">
+            <Table aria-label="simple table">
+              <TableBody>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">作品特色</TableCell>
+                  <TableCell>{signature}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">建議改編領域</TableCell>
+                  <TableCell>{field}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">建議改編規格</TableCell>
+                  <TableCell>{spec}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell component="th" scope="row" className="column is-4 is-offset-1">改編參考</TableCell>
+                  <TableCell><Link to={`https://zh.wikipedia.org/wiki/${refs}`}>{refs}</Link></TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       </div>
     </section>
@@ -104,9 +196,20 @@ const BlogPost = ({ data }) => {
             />
           </Helmet>
         }
+        dev={post.frontmatter.dev}
+        refs={post.frontmatter.refs}
         cats={post.frontmatter.cats}
         tags={post.frontmatter.tags}
+        year={post.frontmatter.year}
+        spec={post.frontmatter.spec}
         title={post.frontmatter.title}
+        owner={post.frontmatter.owner}
+        field={post.frontmatter.field}
+        author={post.frontmatter.author}
+        origin={post.frontmatter.origin}
+        property={post.frontmatter.property}
+        publisher={post.frontmatter.publisher}
+        signature={post.frontmatter.signature}
         featuredimage={post.frontmatter.featuredimage}
       />
     </Layout>
@@ -131,6 +234,17 @@ export const pageQuery = graphql`
         logline
         cats
         tags
+        author
+        origin
+        publisher
+        year
+        owner
+        dev
+        property
+        signature
+        field
+        spec
+        refs
         featuredimage {
           childImageSharp {
             fluid(maxWidth: 250, quality: 100) {
