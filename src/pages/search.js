4f053c74b0965f993da255e391ad0588d5e6f2d9
Summit Suen <summit.suen@gmail.com>
2020-10-28 04:57:38 +0800

    Adjust import

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { graphql } from 'gatsby'
 import Layout from "../components/layout";
 import SearchForm from '../components/searchForm';
 import SearchResults from '../components/searchResults';
