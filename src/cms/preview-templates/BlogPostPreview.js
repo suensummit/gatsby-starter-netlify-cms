fad5ee10255fb0de0d20f59f9701c7aec45da2bc
Summit Suen <summit.suen@gmail.com>
2020-10-29 17:06:35 +0800

    Try CMS

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -4,12 +4,16 @@ import { BlogPostTemplate } from '../../templates/blog-post'
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
   const tags = entry.getIn(['data', 'tags'])
+  const cats = entry.getIn(['data', 'cats'])
   return (
     <BlogPostTemplate
-      content={widgetFor('body')}
+      // content={widgetFor('內容')}
       logline={entry.getIn(['data', 'logline'])}
       tags={tags && tags.toJS()}
-      title={entry.getIn(['data', 'title'])}
+      cats={cats && cats.toJS()}
+      title={entry.getIn(['data', '標題'])}
+      author={entry.getIn(['data', '創作者'])}
+      signature={entry.getIn(['data', '作品特色'])}
     />
   )
 }
